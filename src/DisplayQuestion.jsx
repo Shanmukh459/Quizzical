84ff03e80cd297ee0ed35624252970109e227382
Shanmukh459 <saikrrish459@gmail.com>
2023-08-31 13:04:58 -0700

    changes the title and working on adding selectedAnswer key to the quizData

--- src/DisplayQuestion.jsx
+++ src/DisplayQuestion.jsx
@@ -1,29 +1,32 @@
 import React from "react"
 import he from "he"
+import { nanoid } from 'nanoid'
 
 export default function DisplayQuestion(props) {
-    const [item, setItem] = React.useState(props.item)
+    //const [item, setItem] = React.useState(props.item)
     let options = props.item.incorrect_answers
     const correctOption = props.item.correct_answer
     
     options.length < 4 && options.splice((options.length+1) * Math.random() | 0, 0, correctOption)
 
-    function handleClick(event) {
-        setItem(prevItem => {
-            return {
-                ...prevItem,
-                selected_answer: event.target.innerText
-            }
-        })
-        console.log(event.target.innerText)
-    }
 
-    const optionsElements = options.map(option => <button onClick={handleClick} key={option}>{option}</button>)
+    const optionsElements = options.map(option => {
+        const incorrectAnswerClassName = `
+        ${props.item.selectedAnswer === option ? "option-btn-selected" : "option-btn"}`
+        return(
+            <button 
+                onClick={() => props.handleClick(props.item.id, option)} 
+                key={nanoid()}
+                className={incorrectAnswerClassName}
+            >{he.decode(option)}</button>
+        )
+        
+    })
 
     return (
         <>
             <p>{he.decode(props.item.question)}</p>
-            <div>{optionsElements}</div>
+            {optionsElements}
         </>
         
     )
--- src/QuizScreen.jsx
+++ src/QuizScreen.jsx
@@ -1,24 +1,47 @@
 import React from "react"
 import DisplayQuestion from "./DisplayQuestion"
+import { nanoid } from "nanoid"
 
 export default function QuizScreen() {
+    const [isGameOver, setIsGameOver] = React.useState(false)
     const [quizData, setQuizData] = React.useState([])
 
     React.useEffect(() => {
-        fetch("https://opentdb.com/api.php?amount=5")
+        fetch("https://opentdb.com/api.php?amount=5&type=multiple")
             .then(res => res.json())
-            .then(data => setQuizData(data.results))
+            .then(data => setQuizData(data.results.map(question => {
+                return {
+                    ...question,
+                    id: nanoid(),
+                    selectedAnswer: " "
+                }
+            })))
+        console.log(`before select: ${quizData.selectedAnswer}`)
     }, [])
+
+    function handleSelectAnswer(id, option) {
+        console.log(typeof(option))
+        setQuizData(prevQuestions => (
+            prevQuestions.map(question => {
+                return id === question.id ? {...question, selectedAnswer: option} : question
+            })
+        ))
+        console.log("After select " + quizData)
+    }
 
     const questionElements = quizData.map((item) => {
         return (
-            <DisplayQuestion key={item.question} item={item}/>
+            <DisplayQuestion key={item.question} item={item} handleClick={handleSelectAnswer}/>
         )
     })
-    
+
     return (
+        <>
         <div>
             {questionElements}
         </div>
+        <button>Check Answers</button>
+        </>
+        
     )
 }
